048b3a355ca3406684ea4f77b9d6dc039d40a86a
Irina Rusinova <rusinovarusya@mail.ru>
2023-07-10 13:51:25 +0300

    feat: add simple todo

---
+++ todo-script.js
@@ -0,0 +1,29 @@
+const tasksList = document.querySelector(".tasks-list");
+const inputNewTask = document.querySelector(".input-new-task");
+
+
+function handleKeydownEnter(event) {
+  if (event.key === "Enter") {
+    if (inputNewTask.value === "") {
+      alert("fill in");
+    } else {
+      createNewTask(event);
+    }
+  }
+}
+
+function createNewTask() {
+  const text = inputNewTask.value;
+  const id = new Date().getTime();
+
+  tasksList.insertAdjacentHTML("beforeend", 
+  `<div class="task-item">
+    <input type="checkbox" name="pomodoro-${document.querySelector(".pomodoro-num")}" id="${id}">
+    <label for="${id}">${text}</label>
+    <button class="btn-delete-task">del</button>
+  </div>`);
+
+  inputNewTask.value = "";
+}
+
+inputNewTask.addEventListener("keydown", handleKeydownEnter);
